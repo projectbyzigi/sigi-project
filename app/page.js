86981b696cefbe5824eea2f59c95700a9e7e64e4
colinboro9-spec <colinboro9@gmail.com>
2026-06-24 18:50:47 +0200

    First SIGI website

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="content">
+      <h1 className="brand">SIGI</h1>
+      <p className="headline">Etwas Neues entsteht.</p>
+      <p className="description">
+        Unsere Website befindet sich derzeit im Aufbau.
+        <br />
+        Wir arbeiten an etwas Besonderem und sind bald zurück.
+      </p>
+    </main>
+  );
+}
